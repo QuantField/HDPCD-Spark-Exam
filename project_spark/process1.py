6ed4c752396f37adf2d4b4f55097ddb392d97cbf
Kamel Saadi <saadi.kamel@gmail.com>
2018-09-11 20:31:16 +0100

    Commit 10

--- project_spark/process1.py
+++ project_spark/process1.py
@@ -1,7 +1,10 @@
+# the data can be downloaded from 
+# https://drive.google.com/open?id=1Hid-RrefaSjgQGGNDcvhD-R-rIv9GX0u
+#
 from pyspark.sql import SQLContext,Row
 sqC = SQLContext(sc)
 
-rdd      = sc.textFile("tv_view.csv")
+rdd      =sc.textFile("/home/kamel_dev/spData/tv_view.csv")
 firstRow = rdd.first()
 
 def del_double_quotes(x):
@@ -20,5 +23,35 @@ df = rddData.map(lambda line : line.split(","))\
                 line[6],line[7],line[8],line[9],line[10],
                 line[11],line[12],line[13],line[14],line[15])).toDF(header)
 
+number_of_rows = df.count() #3863142            
 
 housid = df.groupBy('household_id').count()            
+
+df_train = df[df['dob']!='1900-01-01']
+df_test  = df[df['dob']=='1900-01-01']
+
+n_tr = df_train.count()
+n_te = df_test.count()
+
+ds = df.select('household_id').distinct()
+>>> ids.head()
+Row(household_id=u'432216801')                                                  
+>>> ids.count()
+2813                                                                            
+>>> 
+>>> df.registerTempTable("myTab")
+>>> sqC.sql("select count(*) as cnt from myTab")
+DataFrame[cnt: bigint]
+>>> sqC.sql("select count(*) as cnt from myTab").show()
++-------+                                                                       
+|    cnt|
++-------+
+|3863142|
++-------+
+
+
+
+
+
+
+
